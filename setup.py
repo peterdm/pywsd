3af1302a359743e29eb9d581108acdf230544b9e
Peter Dixon-Moses <peter@tatooine.home>
2014-08-01 12:42:29 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pywsd',
+    version='0.1',
+    packages=['pywsd',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='Python Implementations of Word Sense Disambiguation (WSD) technologies -- forked from github.com/alvations/pywsd',
+)
